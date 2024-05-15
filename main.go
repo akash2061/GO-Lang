6c4f19b1db246315528b80606abcbdf3757dd36f
akash2061 <lucifermorningstar2061@gmail.com>
2024-05-15 20:38:55 +0530

    String

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+var text = "This is a global text"
+
 func main() {
 	fmt.Println("Hello... Go...!")
 
@@ -10,5 +12,5 @@ func main() {
 	var name2 string
 	name2 = "Foo..."
 	name3 := "Hello"
-	fmt.Println(name,"\n", name1,"\n", name2,"\n", name3)
+	fmt.Println(name, "\n", name1, "\n", name2, "\n", name3, "\n", text)
 }
