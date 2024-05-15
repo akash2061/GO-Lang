27e1561af42690e9ffb30f7addfa809689a0ce5a
akash2061 <lucifermorningstar2061@gmail.com>
2024-05-15 20:35:08 +0530

    String

--- main.go
+++ main.go
@@ -6,5 +6,9 @@ func main() {
 	fmt.Println("Hello... Go...!")
 
 	var name string = "Morningstar_2061"
-	fmt.Println(name)
+	var name1 = "akash2061"
+	var name2 string
+	name2 = "Foo..."
+	name3 := "Hello"
+	fmt.Println(name,"\n", name1,"\n", name2,"\n", name3)
 }
