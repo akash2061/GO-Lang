061c7ec32c1a6ba8dca053152657601c87e2b8cb
akash2061 <lucifermorningstar2061@gmail.com>
2024-05-22 19:27:04 +0530

    Math

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"        //! Read-Input
 	"fmt"          //! Print-Format
 	"log"          //! For Errors
+	"math"         //! Math-lib
 	"math/rand"    //! Random Numbers
 	"os"           //! Input-Output
 	"reflect"      //! TypeOf
@@ -119,4 +120,27 @@ func main() {
 	rand.Seed(seedSecs)
 	randNum := rand.Intn(50) + 1
 	pl("\nRandom :", randNum)
+	
+	//! There are many math functions
+	pl("\nMath :")
+	pl("Abs(-10) =", math.Abs(-10))
+	pl("Pow(4, 2) =", math.Pow(4, 2))
+	pl("Sqrt(16) =", math.Sqrt(16))
+	pl("Cbrt(8) =", math.Cbrt(8))
+	pl("Ceil(4.4) =", math.Ceil(4.4))
+	pl("Floor(4.4) =", math.Floor(4.4))
+	pl("Round(4.4) =", math.Round(4.4))
+	pl("Log2(8) =", math.Log2(8))
+	pl("Log10(100) =", math.Log10(100))
+	// Get the log of e to the power of 2
+	pl("Log(7.389) =", math.Log(math.Exp(2)))
+	pl("Max(5,4) =", math.Max(5, 4))
+	pl("Min(5,4) =", math.Min(5, 4))
+
+	// Convert 90 degrees to radians
+	r90 := 90 * math.Pi / 180
+	// Convert 1.5708 radians to degrees
+	d90 := r90 * (180 / math.Pi)
+	fmt.Printf("%f radians = %f degrees\n", r90, d90)
+	pl("Sin(90) =", math.Sin(r90))
 }
