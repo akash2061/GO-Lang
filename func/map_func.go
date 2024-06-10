a48cdd489a0221e12383518148a5607364a14d66
akash2061 <lucifermorningstar2061@gmail.com>
2024-06-10 22:40:55 +0530

    Map-access

--- func/map_func.go
+++ func/map_func.go
@@ -14,9 +14,16 @@ func Maps() string {
 	// values = make(map[int8]string)
 	// Values := make(map[int8]string)
 
-	values := make(map[int8]string)
+	cmd := make(map[string]string)
+	cmd["h"] = "help"
+	cmd["v"] = "version"
 
-	values[1] = "one"
+	values := map[int]string{1: "One", 2: "Two", 3: "Three"}
+
+	fmt.Printf("Values: %v\n", values)
+	pl("Out of range access :", values[4])
+	_, ok := values[4]
+	pl("value ok:", ok)
 
 	return "Hello Maps"
 }
