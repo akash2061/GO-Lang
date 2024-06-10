63172894955d846ec8a414bd1b0f8e37bbc4dcd9
akash2061 <lucifermorningstar2061@gmail.com>
2024-06-10 22:30:47 +0530

    Map-Values

--- func/map_func.go
+++ func/map_func.go
@@ -10,5 +10,13 @@ func Maps() string {
 
 	//! var MyMap map [keyType]valueType
 
+	// var values map[int8]string
+	// values = make(map[int8]string)
+	// Values := make(map[int8]string)
+
+	values := make(map[int8]string)
+
+	values[1] = "one"
+
 	return "Hello Maps"
 }
