489f675e5061fb80247d4ddba53399c50fe147f9
akash2061 <lucifermorningstar2061@gmail.com>
2024-06-11 23:19:44 +0530

    Update

--- functions/structure.go
+++ functions/structure.go
@@ -16,17 +16,21 @@ func Str() {
 	var c1 custom
 	c1.name = "Vincent-V"
 	c1.addr = "8 Megabuilding H10, NC"
-	c1.bal = 15324.609
+	c1.bal = 15_324.609
 
 	cInfo(c1)
 	pl()
 	uAddr(&c1, "Motel Kabuki Market, NC")
 	pl(c1.name+" completing gigs at Address :", c1.addr)
+
+	v := custom{"Valerie-V", "Corpo-Plaza, City Center, NC", 1_000_000.00}
+	pl("\nNew-User:")
+	cInfo(v)
 }
 
 func cInfo(c custom) {
 	fmt.Printf("%s owns : %.2f Eddied.\n", c.name, c.bal)
-	fmt.Printf("%s lives at %s\n", c.name, c.addr)
+	fmt.Printf("%s lives at \"%s\"\n", c.name, c.addr)
 }
 func uAddr(c *custom, addrs string) {
 	c.addr = addrs
