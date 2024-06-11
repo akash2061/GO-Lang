455dc8a2cbe1ac47a759e468e8e96568381fad61
akash2061 <lucifermorningstar2061@gmail.com>
2024-06-11 22:21:05 +0530

    My-Constraint

--- functions/generics.go
+++ functions/generics.go
@@ -5,15 +5,27 @@ import (
 	"time"
 )
 
+type Constraint interface {
+	int | float64
+}
+
+func Sum[T Constraint](x T, y T) T {
+	return x + y
+}
+
 func Gene() {
-	fmt.Print(gene())
+	fmt.Print(great())
 	for i := 0; i < 3; i++ {
 		time.Sleep(500 * time.Millisecond)
 		fmt.Print(".")
 	}
-	fmt.Println("\nHii")
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("\nHii Generics...!")
+
+	pl("Adding int: ", Sum(5, 4))
+	pl("Adding float64: ", Sum(5.912, 4.726493))
 }
 
-func gene() string {
+func great() string {
 	return "Calling Generics"
 }
