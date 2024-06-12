f9eee1cf4937dd627f778eaefe6cdf7abc787d87
akash2061 <lucifermorningstar2061@gmail.com>
2024-06-12 23:17:17 +0530

    User Defined Datatypes

--- custom/user_defined_types.go
+++ custom/user_defined_types.go
@@ -1,5 +1,32 @@
 package functions
 
+import "fmt"
+
+type Tsp float64
+type TBs float64
+type ML float64
+
+// Convert with functions (Bad Way)
+// func tspToML(tsp Tsp) ML {
+// 	return ML(tsp * 4.92)
+// }
+
+// func TBToML(tbs TBs) ML {
+// 	return ML(tbs * 14.79)
+// }
+
+// Associate method with types
+func (tsp Tsp) ToMLs() ML {
+	return ML(tsp * 4.92)
+}
+func (tbs TBs) ToMLs() ML {
+	return ML(tbs * 14.79)
+}
+
 func Udt() {
 	pl("\nUser Defined Datatypes...\n")
+	ml1 := Tsp.ToMLs(3)
+	fmt.Printf("3 tps = %.2f mL\n", ml1)
+	ml1 = TBs.ToMLs(2)
+	fmt.Printf("2 tbs = %.2f mL\n", ml1)
 }
