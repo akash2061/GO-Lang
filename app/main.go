5d46a090e1903304da09ad766b5a77db91901f5b
akash2061 <lucifermorningstar2061@gmail.com>
2024-06-12 21:58:47 +0530

    utiliy-update

--- app/main.go
+++ app/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	packet "example/project/package"
+	utils "example/project/utils"
 	"fmt"
 	"reflect"
 )
 
 func main() {
-	fmt.Println("Hello", packet.Name)
+	fmt.Println("Hello", utils.Name)
 	intArr := []int{2, 3, 5, 7, 11}
-	strArr := packet.IntArrToStrArr(intArr)
+	strArr := utils.IntArrToStrArr(intArr)
 	fmt.Println(strArr)
 	fmt.Println(reflect.TypeOf((strArr)))
 }
--- app/utils/utils.go
+++ app/utils/utils.go
@@ -1,4 +1,4 @@
-package packet
+package utils
 
 import (
 	"strconv"
