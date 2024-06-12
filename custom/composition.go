cf8d6e99a97127c6629b0018714a934da99f7b0c
akash2061 <lucifermorningstar2061@gmail.com>
2024-06-12 22:16:43 +0530

    Composition

--- custom/composition.go
+++ custom/composition.go
@@ -5,8 +5,31 @@ import (
 	"time"
 )
 
+type contact struct {
+	Name   string
+	number string
+}
+type business struct {
+	name string
+	addr string
+	contact
+}
+
+func (b business) info() {
+	fmt.Printf("Contact: %s at %s", b.contact.Name, b.name)
+}
 func Compos() {
-	cmp_great()
+	pl(cmp_great())
+	c1 := contact{
+		"V",
+		"555-1212",
+	}
+	b1 := business{
+		"Arasaka-Corporation",
+		"City Center, NC",
+		c1,
+	}
+	b1.info()
 }
 
 func cmp_great() string {
